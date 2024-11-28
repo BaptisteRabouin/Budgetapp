36bb55b6463c3ee3817e55fdca45acf3661f611c
root <root@DevOps.home>
2024-11-28 12:57:25 +0000

    modification total revenus

--- app/forms.py
+++ app/forms.py
@@ -3,13 +3,13 @@ from wtforms import StringField, DecimalField, DateField, SubmitField
 from wtforms.validators import DataRequired, Length, NumberRange
 
 class ChargeForm(FlaskForm):
-    description = StringField('Description', validators=[DataRequired(), Length(max=128)])
+    description = StringField('Nom', validators=[DataRequired(), Length(max=128)])
     amount = DecimalField('Montant', validators=[DataRequired(), NumberRange(min=0)])
     date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
     submit = SubmitField('Ajouter Charge')
 
 class RevenueForm(FlaskForm):
-    description = StringField('Description', validators=[DataRequired(), Length(max=128)])
+    description = StringField('Nom', validators=[DataRequired(), Length(max=128)])
     amount = DecimalField('Montant', validators=[DataRequired(), NumberRange(min=0)])
     date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
     submit = SubmitField('Ajouter Revenu')
